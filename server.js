0902e5db51f4d977cd285e4002aee5f0268b20d5
Ricky Huang <rickyhuang53@gmail.com>
2026-05-08 18:45:06 +0800

    Serve sitemap and robots with correct content types

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ const contentTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "application/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
   ".jpg": "image/jpeg",
   ".jpeg": "image/jpeg",
   ".png": "image/png",
